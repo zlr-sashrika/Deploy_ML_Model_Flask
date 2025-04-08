633cb09493b19c47b6efada07d16236f5d4ba44d
sashrikazelar <sashrika@zelarsoft.com>
2025-04-08 17:28:25 +0530

    'pushingchanges'

--- application.py
+++ application.py
@@ -23,4 +23,4 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
